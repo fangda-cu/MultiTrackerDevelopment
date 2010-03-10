dcd6dad9a577d555b4b2e8a50c7ea00986295a51
Miklos Bergou <miklos@acer.(none)>
2010-03-10 13:17:27 -0500

    added setup script

---
+++ setupEnvironment-Miklos.sh
@@ -0,0 +1,4 @@
+export MAYA_LOCATION=/usr/autodesk/maya
+export MAYA_PLUG_IN_PATH=`pwd`/Apps/WmBunsen/:${MAYA_PLUG_IN_PATH}
+export MAYA_SCRIPT_PATH=`pwd`/../Apps/WmBunsen/:${MAYA_SCRIPT_PATH}
+export XBMLANGPATH=`pwd`/../Apps/WmBunsen/icons/%B:${XBMLANGPATH}
