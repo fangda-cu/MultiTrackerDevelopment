bf4334324ac1c5aa4f3042573efbc4157ac46113
Eitan Grinspun <egrinspun@irishpeach.wetafx.co.nz>
2011-04-21 17:07:34 +1200

    arrowheads removed

--- BASim/src/Render/RenderUtils.hh
+++ BASim/src/Render/RenderUtils.hh
@@ -19,8 +19,8 @@ inline void drawArrow(const Vec3d& start, Vec3d dir, Scalar scale = 1.0)
   Scalar len = (end - start).norm();
   Scalar rad = 0.03 * len;
 
-  Vec3d x;
-  findOrthogonal(x, end - start);
+  Vec3d x(0,0,0);
+  if (len > 0) findOrthogonal(x, end - start);
   Vec3d y = (end - start).cross(x).normalized();
 
   glBegin(GL_QUAD_STRIP);
@@ -38,16 +38,16 @@ inline void drawArrow(const Vec3d& start, Vec3d dir, Scalar scale = 1.0)
   }
   glEnd();
 
-  Vec3d center = start + 0.8 * (end - start);
-  Vec3d orientation = (end - start).normalized();
-  Vec3d axis = Vec3d(0,0,1).cross(orientation);
-  Scalar angle = 180.0/M_PI*atan2(axis.norm(), Vec3d(0,0,1).dot(orientation));
-  if (angle != 0) axis.normalize();
-  glPushMatrix();
-  glTranslated(center(0), center(1), center(2));
-  glRotated(angle, axis(0), axis(1), axis(2));
-  glutSolidCone(2.2 * rad, 0.2 * len, 32, 2);
-  glPopMatrix();
+  //Vec3d center = start + 0.8 * (end - start);
+  //Vec3d orientation = (end - start).normalized();
+  //Vec3d axis = Vec3d(0,0,1).cross(orientation);
+  //Scalar angle = 180.0/M_PI*atan2(axis.norm(), Vec3d(0,0,1).dot(orientation));
+  //if (angle != 0) axis.normalize();
+  //  glPushMatrix();
+  // glTranslated(center(0), center(1), center(2));
+  //glRotated(angle, axis(0), axis(1), axis(2));
+  //glutSolidCone(2.2 * rad, 0.2 * len, 32, 2);
+  //glPopMatrix();
 }
 
 } // namespace BASim
