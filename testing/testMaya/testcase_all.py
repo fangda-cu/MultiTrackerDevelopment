fee7d84ab68f451826a5f68949c29d7fafb926ee
Susan Lynn Howard <showard@yateley.wetafx.co.nz>
2011-05-09 12:15:03 +1200

    Removed case 9c refs #23111

--- testing/testMaya/testcase_all.py
+++ testing/testMaya/testcase_all.py
@@ -221,23 +221,6 @@ class MayaTest( TestCase ):
 			print maya.cmds.getAttr( 'wmFigaroNode1.syncAttrs' )
 			print "Test Case 9b: worked."
 			
-"""			
-	def testcase9c( self ):
-               
-		self.shell.need( 'maya' , '2010_64' )
-		self.shell.call('bob_check_opt' )
-		print 'set needs'
-		
-		with self.shell.mayaSession() as maya:
-			maya.cmds.file( '/weta/dev/user/showard/shots/maya/scenes/fur/TEST_CASES/case9c.mb', open=True, force=True )
-			for i in range(1,23):
-				maya.cmds.currentTime( i, edit=True )
-				print( 'time %d'%i )
-				print maya.cmds.getAttr( 'wmFigaroNode1.syncAttrs' )
-			maya.cmds.currentTime( 1, edit=True )
-			print maya.cmds.getAttr( 'wmFigaroNode1.syncAttrs' )
-			print "Test Case 9c: worked."
-"""
 			
 	def testcase10( self ):
                
