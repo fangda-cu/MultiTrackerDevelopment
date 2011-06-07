daccce75abf80548829757d81bc6c94b8fbf0fc0
Jean-Marie Aubry <jaubry@wetafx.co.nz>
2011-06-07 17:45:05 +1200

    Finished complete treatment of stretching failure

--- BASim/src/Physics/ElasticRods/BARodStepper.cc
+++ BASim/src/Physics/ElasticRods/BARodStepper.cc
@@ -877,24 +877,20 @@ void BARodStepper::step(RodSelectionType& selected_rods)
         bool collisionFailure = failed_collisions_rods[rodidx];
         bool stretching = stretching_rods[rodidx];
 
-        //	std::cout << "rod " << rodidx << ": solve " << (solveFailure ? "FAILED " : "ok ")
-        //		  << "collisions " << (collisionFailure ? "FAILED " : "ok ")
-        //		  << "explosion-check " << (explosion ? "FAILED " : "ok ");
+        bool substep = (solveFailure && m_level < m_perf_param.m_solver.m_max_substeps) //
+                || (explosion && m_level < m_perf_param.m_explosion.m_max_substeps)//
+                || (collisionFailure && m_level < m_perf_param.m_collision.m_max_substeps)//
+                || (stretching && m_level < m_perf_param.m_stretching.m_max_substeps);
 
-        bool substep = (solveFailure && m_level < m_perf_param.m_solver.m_max_substeps) || (explosion && m_level
-                < m_perf_param.m_explosion.m_max_substeps) || (collisionFailure && m_level
-                < m_perf_param.m_collision.m_max_substeps);
+        bool killRod = (solveFailure && m_perf_param.m_solver.m_in_case_of == FailureMode::KillTheRod) //
+                || (explosion && m_perf_param.m_explosion.m_in_case_of == FailureMode::KillTheRod)//
+                || (collisionFailure && m_perf_param.m_collision.m_in_case_of == FailureMode::KillTheRod)//
+                || (stretching && m_perf_param.m_stretching.m_in_case_of == FailureMode::KillTheRod);
 
-        bool killRod = (solveFailure && m_perf_param.m_solver.m_in_case_of == FailureMode::KillTheRod)
-                || (explosion && m_perf_param.m_explosion.m_in_case_of == FailureMode::KillTheRod)
-                || (collisionFailure && m_perf_param.m_collision.m_in_case_of == FailureMode::KillTheRod)
-                || stretching && true; // Shall we pass a parameter for the stretching failure mode?
-
-        bool haltSim =
-                (solveFailure && m_perf_param.m_solver.m_in_case_of == FailureMode::HaltSimulation)
-                        || (explosion && m_perf_param.m_explosion.m_in_case_of
-                                == FailureMode::HaltSimulation) || (collisionFailure
-                        && m_perf_param.m_collision.m_in_case_of == FailureMode::HaltSimulation);
+        bool haltSim = (solveFailure && m_perf_param.m_solver.m_in_case_of == FailureMode::HaltSimulation)//
+                || (explosion && m_perf_param.m_explosion.m_in_case_of == FailureMode::HaltSimulation) //
+                || (collisionFailure && m_perf_param.m_collision.m_in_case_of == FailureMode::HaltSimulation)//
+                || (stretching && m_perf_param.m_stretching.m_in_case_of == FailureMode::HaltSimulation);
 
         if (substep) // Only in that case keep the rod in the selected list
             continue;
@@ -907,7 +903,7 @@ void BARodStepper::step(RodSelectionType& selected_rods)
                 m_num_explosion_killed++;
             if (collisionFailure && m_perf_param.m_collision.m_in_case_of == FailureMode::KillTheRod)
                 m_num_collision_killed++;
-            if (stretching && true)
+            if (stretching && m_perf_param.m_stretching.m_in_case_of == FailureMode::KillTheRod)
                 m_num_stretching_killed++;
 
             rod_kill++;
