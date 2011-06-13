32c6a0bf2c5f8c726bbe0d3f646bfb39b0005f73
Eitan Grinspun <egrinspun@irishpeach.wetafx.co.nz>
2011-06-13 20:19:21 +1200

    forgot to commit RodExternalConservativeForce.hh

---
+++ BASim/src/Physics/ElasticRods/RodExternalConservativeForce.hh
@@ -0,0 +1,38 @@
+/**
+ * \file RodExternalConservativeForce.hh
+ *
+ * \author eitan@cs.columbia.edu
+ * \date 06/06/2011
+ */
+
+#ifndef RODEXTERNALCONSERVATIVEFORCE_HH
+#define RODEXTERNALCONSERVATIVEFORCE_HH
+
+namespace BASim
+{
+
+class ElasticRod;
+class MatrixBase;
+
+/** Base class for external forces applied to a rod. */
+class RodExternalConservativeForce : public RodExternalForce
+{
+public:
+
+    explicit RodExternalConservativeForce(bool implicit = true)
+    {
+      m_conservative = true;
+    }
+
+    virtual ~RodExternalConservativeForce()
+    {
+    }
+
+    virtual Scalar computeEnergy(const ElasticRod& rod) const = 0;
+ 
+    virtual void computeForceEnergy(const ElasticRod& rod, VecXd& force, Scalar& energy) const = 0;
+};
+
+} // namespace BASim
+
+#endif // RODEXTERNALCONSERVATIVEFORCE_HH
