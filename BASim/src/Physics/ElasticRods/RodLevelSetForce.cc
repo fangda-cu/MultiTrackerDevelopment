f2ad8e96326481ee5bcb76af8914e7a6fec36277
Eitan Grinspun <egrinspun@irishpeach.wetafx.co.nz>
2011-06-13 20:34:16 +1200

    new files for level set penalty force

---
+++ BASim/src/Physics/ElasticRods/RodLevelSetForce.cc
@@ -0,0 +1,10 @@
+// RodLevelSetForce.cc
+//
+
+//#include "BASim/src/Collisions/CollisionMeshData.hh"
+#include "RodLevelSetForce.hh"
+#ifdef WETA
+#include "../../Math/Math.hh"
+#else
+#include "BASim/src/Math/Math.hh"
+#endif
