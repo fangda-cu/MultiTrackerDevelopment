7d3ae01a1307cf7be5ed4e3ce140039b87444c93
Eitan Grinspun <egrinspun@irishpeach.wetafx.co.nz>
2011-06-14 09:14:39 +1200

    using identity spectral shift

--- BASim/src/Physics/ElasticRods/RodLevelSetForce.hh
+++ BASim/src/Physics/ElasticRods/RodLevelSetForce.hh
@@ -1,33 +1,22 @@
-// RodRodPenaltyForce.hh
+// RodLevelSetForce.hh
 //
 
-#ifndef RodPenaltyForce_HH
-#define RodPenaltyForce_HH
+#ifndef RodLevelSetForce_HH
+#define RodLevelSetForce_HH
 
 #include "ElasticRod.hh"
 #include "RodExternalForce.hh"
-//#include "../../Collisions/BVHAABB.hh"
-#include "../../Collisions/Collision.hh"
-
-//#include <ext/hash_map>
-#include <map>
 
 namespace BASim
 {
 
-class RodPenaltyForce: public RodExternalForce
+class RodLevelSetForce: public RodExternalForce
 {
 public:
-    RodPenaltyForce(double penaltyThicknessFraction = 0.1);
-    ~RodPenaltyForce();
+    RodLevelSetForce(double LevelSetThicknessFraction = 0.1);
+    ~RodLevelSetForce();
 
     virtual void computeForce(const ElasticRod& rod, VecXd& F) const;
-
-    void registerProximityCollision(int vertex, VertexFaceProximityCollision* vfpcol);
-
-    bool cleared() const;
-
-    void clearProximityCollisions();
 
     virtual void computeForceDX(int baseindex, const ElasticRod& rod, Scalar scale, MatrixBase& J) const;
 
@@ -37,12 +26,6 @@ public:
 
 protected:
     void localJacobian(MatXd& J, const Scalar stiffness, const Vec3d& normal) const;
-
-    const double m_penaltyThicknessFraction; // fraction of proximity threshold to use for penalty thickness
-
-    std::vector<int> vidx;
-
-    std::vector<const VertexFaceProximityCollision*> vertex_face_collisions;
 
 };
 
