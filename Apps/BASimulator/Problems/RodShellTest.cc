8df97115124f9c4f037fde48b6fca97fa392abe8
Fang Da <fang@cs.columbia.edu>
2012-05-13 23:58:15 -0400

    Changed twist constraint direction, to match problem 1

--- Apps/BASimulator/Problems/RodShellTest.cc
+++ Apps/BASimulator/Problems/RodShellTest.cc
@@ -479,7 +479,7 @@ void RodShellTest::setupScene2()
   obj->constrainVertex(vertex_handles[nv - 1], positions[vertex_handles[nv - 1]]);  // fix tail position
   obj->constrainVertex(vertex_handles[nv - 2], positions[vertex_handles[nv - 2]]);  // fix tail tangent
   rod->constrainEdge(rodEdges[0], 0);      // no twist at the head
-  rod->constrainEdgeVel(rodEdges[nv - 2], 0, 1, 0);  // twist rate = 1 at the tail
+  rod->constrainEdgeVel(rodEdges[nv - 2], 0, -1, 0);  // twist rate = 1 at the tail
   
   // create an empty shell model
   FaceProperty<char> shellFaces(obj); 
