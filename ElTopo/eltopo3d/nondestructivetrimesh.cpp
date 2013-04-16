c3b16e644fafed6cb75abd180991c4f0c0b9183a
Christopher Batty <christopherbatty@yahoo.com>
2013-04-16 13:33:43 -0400

    Remove old incorrect comment.

--- ElTopo/eltopo3d/nondestructivetrimesh.cpp
+++ ElTopo/eltopo3d/nondestructivetrimesh.cpp
@@ -154,14 +154,6 @@ size_t NonDestructiveTriMesh::nondestructive_add_triangle( const Vec3st& tri, co
     size_t idx = m_tris.size();
     m_tris.push_back(tri);
     m_triangle_to_edge_map.resize(idx+1);
-    
-    ////////////////////////////////////////////////////////////
-    // FD 20121126
-    //
-    // create a placeholder face label (this is not made a 
-    //  function argument because we want to minimize changes
-    //  to El Topo)
-    //
     m_triangle_labels.push_back(label);
     
     ////////////////////////////////////////////////////////////
