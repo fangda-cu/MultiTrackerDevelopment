80815acb3a406562955576cc1560456610e63c27
Christopher Batty <christopherbatty@yahoo.com>
2013-04-17 10:15:28 -0400

    Remove old code.

--- ElTopo/eltopo3d/accelerationgrid.cpp
+++ ElTopo/eltopo3d/accelerationgrid.cpp
@@ -302,10 +302,6 @@ bool boxes_overlap(Vec3i low_0, Vec3i high_0, Vec3i low_1, Vec3i high_1) {
 void AccelerationGrid::update_element(size_t idx, const Vec3d& xmin, const Vec3d& xmax)
 {
    
-  /* remove_element(idx);
-   add_element(idx, xmin, xmax);
-   return;*/
-   
    //do an add and remove simultaneously (before we had done them separately, slower)
 
    bool is_new = false;
@@ -461,7 +457,7 @@ void AccelerationGrid::update_element(size_t idx, const Vec3d& xmin, const Vec3d
          }
       }
    }
-
+   
 }
 
 // --------------------------------------------------------
