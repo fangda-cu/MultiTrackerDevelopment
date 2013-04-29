2aea53421cb2799810899a96e1a39c9f1259dc13
Fang Da <fang@cs.columbia.edu>
2013-04-29 19:15:43 -0400

    Fixed a bug due to discrepancy in the criteria for snapping constrainted vertices onto BB wall. This discrepancy caused a triangle to be squeeshed onto the wall but not correctly removed.

--- Apps/BASimulator/Problems/DoubleBubbleTest.cc
+++ Apps/BASimulator/Problems/DoubleBubbleTest.cc
@@ -1011,37 +1011,37 @@ void DoubleBubbleTest::updateBBWallConstraints()
         bool x = false;
         bool y = false;
         bool z = false;
-        if (pos.x() < 1e-4)
+        if (pos.x() < 1e-6)
         {
             pos.x() = 0;
             x = true;
             constraint |= (1 << 0);
         }
-        if (pos.x() > 1 - 1e-4)
+        if (pos.x() > 1 - 1e-6)
         {
             pos.x() = 1;
             x = true;
             constraint |= (1 << 3);
         }
-        if (pos.y() < 1e-4)
+        if (pos.y() < 1e-6)
         {
             pos.y() = 0;
             y = true;
             constraint |= (1 << 1);
         }
-        if (pos.y() > 1 - 1e-4)
+        if (pos.y() > 1 - 1e-6)
         {
             pos.y() = 1;
             y = true;
             constraint |= (1 << 4);
         }
-        if (pos.z() < 1e-4)
+        if (pos.z() < 1e-6)
         {
             pos.z() = 0;
             z = true;
             constraint |= (1 << 2);
         }
-        if (pos.z() > 1 - 1e-4)
+        if (pos.z() > 1 - 1e-6)
         {
             pos.z() = 1;
             z = true;
