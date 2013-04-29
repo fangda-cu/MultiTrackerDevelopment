0fef92f9f8dc78e3d46f6f9530e1fb6413909278
Fang Da <fang@cs.columbia.edu>
2013-04-29 14:38:08 -0400

    dbg

--- ElTopo/eltopo3d/edgecollapser.cpp
+++ ElTopo/eltopo3d/edgecollapser.cpp
@@ -636,7 +636,7 @@ bool EdgeCollapser::collapse_edge( size_t edge )
   if ((dot(rel_vel, edge_vec) > 0 || edge_len >= m_t1_pull_apart_distance) && collapse_will_produce_irregular_junction(edge))
   {
     if (m_surf.m_verbose || eoi)
-      std::cout << "The collapse will produce irregular junction, but the endpoints are moving apart. No need to collapse." << std::endl;
+      std::cout << "The collapse will produce irregular junction, but the endpoints are moving apart. No need to collapse. rv = " << rel_vel << " e = " << edge_vec << std::endl;
     return false;
   }
   
