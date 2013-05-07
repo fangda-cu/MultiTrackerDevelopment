2bdda126f8761cfa3325d8802e691fb381fc8523
Fang Da <fang@cs.columbia.edu>
2013-05-07 15:55:08 -0400

    Allowed to use obj-mode 4 to save .rec only, without dumping OBJs

--- Apps/BASimulator/Problems/DoubleBubbleTest.cc
+++ Apps/BASimulator/Problems/DoubleBubbleTest.cc
@@ -946,7 +946,7 @@ void DoubleBubbleTest::AtEachTimestep()
                 
             }
             
-        } else
+        } else if (RENDER_METHOD == 3)
         {
             g_shell_renderer->turnOffAllRegions();
             
